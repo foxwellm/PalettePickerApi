0b41ec05222010a742bf48ab28136d6ba25fb8ff
Karin Ohman <kaohma@gmail.com>
2019-03-25 13:22:12 -0600

    Update post endpoints and tests to send back entire new project/palette

--- app.js
+++ app.js
@@ -74,27 +74,27 @@ app.post('/api/v1/projects', async (req, res) => {
     const dupProject = await database('projects').where({ name });
     if (dupProject.length) return res.status(409).json(`Project name ${name} already exists.`);
     const project = { name };
-    const newProjectId = await database('projects').insert(project, 'id');
-    return res.status(201).json(newProjectId);
+    const newProject = await database('projects').insert(project).returning('*');
+    return res.status(201).json(newProject);
   } catch (error) {
     return res.status(500).json({ error });
   }
 });
 
 app.post('/api/v1/palettes', async (req, res) => {
-  const newPalette = req.body;
+  const palette = req.body;
   for (let requiredParameter of ['name', 'color1', 'color2', 'color3', 'color4', 'color5', 'project_id']) {
-    if (!newPalette[requiredParameter]) {
+    if (!palette[requiredParameter]) {
       return res.status(422)
         .json(`Expected format: { name: <String>, color1: <String>, color2: <String>, color3: <String>, color4: <String>, color5: <String>, project_id: <Number>}. You're missing a ${requiredParameter} property.`);
     }
   }
   try {
-    const { name, project_id } = newPalette;
+    const { name, project_id } = palette;
     const dupPalette = await database('palettes').where({ name, project_id });
     if (dupPalette.length) return res.status(409).json(`Conflict. palette name ${name} already exists in project id ${project_id}.`);
-    const newPaletteId = await database('palettes').insert(newPalette, 'id')
-    return res.status(201).json(newPaletteId)
+    const newPalette = await database('palettes').insert(palette).returning('*');
+    return res.status(201).json(newPalette)
   } catch (error) {
     return res.status(500).json({ error });
   }
